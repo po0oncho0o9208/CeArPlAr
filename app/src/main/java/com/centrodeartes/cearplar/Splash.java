e24f89b58bb9936e332058e596dc60d1903a6854
po0oncho0o9208 <alfonsojues9208@gmail.com>
2023-03-08 02:46:28 -0600

    Initial commit

---
+++ app/src/main/java/com/centrodeartes/cearplar/Splash.java
@@ -0,0 +1,25 @@
+package com.centrodeartes.cearplar;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, PantallaPrincipal.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1050);
+
+    }
+}
